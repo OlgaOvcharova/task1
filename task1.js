9eafe80938b566d1d096babc4363cb503cda8856
unknown <olga.ovcharova@daxx.com>
2018-11-26 22:24:17 +0200

    new changes

--- task1.js
+++ task1.js
@@ -1,6 +1,21 @@
-let n = 2;
-let result = 1;
-for( let i = n; i <= n*2; i++){
-    resultInter *= i;
-console.info(result);
+function calc(n) {
+    let doubleN = n * 2;
+    
+    let result = 1;
+
+    for (let i = n; i <= doubleN; i++)
+        result *= i;
+   
+    return result;
 }
+
+var n = 3;
+let sum = 0;
+
+for (let index = 1; index <= n; index++) {
+    let result = calc(index);
+    console.info("Result:" + result);
+    sum += result;
+}
+
+console.info("Sum: " + sum);
